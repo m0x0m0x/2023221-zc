541f8a850ef9c6a8ae588ab6a489ba448a524b52
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-02-21 12:29:24 +0000

    ⌛:ZC

--- x/in.sh
+++ x/in.sh
@@ -1,9 +1,12 @@
-#!/bin/bash 
-curl -L https://foundry.paradigm.xyz | bash
-source ~/.bashrc  
-foundryup 
-mkdir -p $HOME/.config/fish/completions
-forge completions fish > $HOME/.config/fish/completions/forge.fish
-cast completions fish > $HOME/.config/fish/completions/cast.fish
-anvil completions fish > $HOME/.config/fish/completions/anvil.fish
-source $HOME/.config/fish/config.fish
+#!/bin/bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/codespace/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/codespace/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get install build-essential
+brew install gcc
+echo "Now run - fish shell - source ~/.profile"
+echo "Now run - Fish shell - source $HOME/.cargo.env" 
+source "/home/codespace/.profile"
+source "$HOME/.cargo/env"
